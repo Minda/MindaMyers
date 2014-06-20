ca4d856b34be7edf794df073d307769cfbb0590b
Minda Myers <minda@stanford.edu>
2014-06-20 14:44:04 -0700

    implemented routing; added portfolio content images

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MindasGotThisUnderControl', ['$scope', function($scope) {
+        $scope.title = "minda";
+
+        console.log("MindaMyers.com debugging ensues...");
+
+        $scope.loadMainContent = function() {
+            console.log("Loading up the main stuffz");
+            $scope.myPage = "partials/main-content.html";
+        }
+
+        $scope.loadBlogContent = function() {
+            console.log("Do you *really* want to know my thoughts?");
+            $scope.myPage = "partials/blog.html";
+        }
+
+        $scope.loadMainContent();
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
