885f9fb9d248c0412b2c27f6ae7d38de7d699c47
Minda Myers <minda@hylo.com>
2015-03-13 22:51:53 -0700

    cleaned up files and started new design

--- js/controllers.js
+++ js/controllers.js
@@ -5,78 +5,15 @@
 angular.module('myApp.controllers', [])
     .controller('MindasGotThisUnderControl', function($scope, $http, $location) {
 
-
-
         $scope.init = function() {
             console.log("MindaMyers.com debugging ensues...");
             console.dir(navigator);
-            $http.get('js/content.json').then(function(res){
+            /*$http.get('js/content.json').then(function(res){
                 $scope.data = res.data;
                 console.dir($scope.data);
             }).then(function() {
                 $scope.featuredWorks = new FeaturedWorks($scope.data);
-            })
+            })*/
 
         }
-
-        $scope.init();
-
-        $scope.loadFeaturedWork = function(featuredWork) {
-            console.log("load featured work:"+featuredWork.title);
-            //load a new page partial
-            console.dir($location);
-        }
-
-        $scope.$on('modeChange', function(event, mass) {
-            console.log(mass);
-            $scope.mode = _.first(mass);
-        });
-
-    })
-    .controller('MainContentController', function($scope) {
-        console.log("Load Main Content");
-        $scope.mode = "main";
-        $scope.$emit('modeChange', [$scope.mode]);
-    })
-    .controller('PortfolioContentController', function($scope) {
-        console.log("Load Portfolio Content");
-        $scope.mode = "portfolio";
-        $scope.$emit('modeChange', [$scope.mode]);
-    })
-    .controller('PortfolioController', function($scope, $route, $routeParams, $location) {
-        console.log("initilazing the portfolio controller");
-
-        $scope.loadPortfolioPiece = function(id) {
-            console.log("load portfolio with id="+id);
-
-            if (id === undefined) {
-                $scope.featuredWork = _.first($scope.featuredWorks.getContents());
-            }
-            else {
-                $scope.featuredWork = $scope.featuredWorks.getFeaturedWorkById(id);
-            }
-            $routeParams.id = id;
-        }
-
-        $scope.loadNextItem = function() {
-            console.log("load next");
-            $scope.loadPortfolioPiece($scope.featuredWorks.getNextIndex());
-        }
-
-        $scope.loadPrevItem = function() {
-            console.log("load prev");
-            $scope.loadPortfolioPiece($scope.featuredWorks.getPrevIndex());
-        }
-
-        $scope.init = function() {
-            console.dir($routeParams);
-            if ($routeParams.id != null) {
-                $scope.featuredWorks.setIndex($routeParams.id);
-            }
-            $scope.loadPortfolioPiece($scope.featuredWorks.getIndex());
-        }
-
-        $scope.init();
-
-
     });
